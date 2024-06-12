4862735bc803c9a1c1348743065be973ca59cd82
Kaushal Parmar <142228942+KaUUshal09@users.noreply.github.com>
2024-06-12 17:50:47 +0530

    index.js

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
-     <div class="main w-full min-h-screen bg-zinc-900">
-        <div class="form px-10 py-10 text-white">
-            <form action="/create" method="post">
-                <input class="block w-full rounded-md px-5 py-2 bg-zinc-800" type="text" placeholder="Title goes here.." name="title">
-                <textarea class="block w-full rounded-md mt-2 px-5 py-2 bg-zinc-800 resize-none" placeholder="Write your task details here.." name="details"></textarea>
-                <input class="mt-2 bg-blue-600 px-5 py-2 rounded-md" type="submit" value="Create Task">
-            </form>
-        </div>
-        <div class="tasks p-10 flex gap-5 flex-wrap"> 
-            <% if(files.length > 0){ %>
-                <% files.forEach(function(val){ %>
-                    <div class=" w-80 px-3 py-4  bg-zinc-800 rounded-md">
-                        <h1 class="text-white text-3xl "><%=  val %></h1>
-                        <div class="flex w-full justify-between item-center mt-3">
-                            <a  class="text-blue-600 inline-block " href="/file/<%= val %>">Read more.</a>
-                            <a  class="text-zinc-600 " href="/edit/<%= val %>">edit</a>
-                            <a class="delete-link text-yellow-600" href="/delete/<%= val %>" data-file="<%= val %>">delete</a>
-                        </div>
-                        
-                    </div>
-                <% }) %>
-            <% }else{ %>
-                <h3 class="text-zinc-600">No tasks yet..</h3>
-            <%  } %>
-           
-            
-        </div>
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
 
-     </div>
+app.use(express.json())
+app.use(express.urlencoded({extends:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine','ejs')
+
+app.get('/',function(req,res){
+    fs.readdir(`./files`,function(err,files){
+        console.log(files)
+        res.render('index',{files:files})
+    })
+})
+
+
+
+app.get('/file/:filename',function(req,res){
+   fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+     res.render('show',{filename:req.params.filename,filedata:filedata})
+   })
+})
+
+app.get('/edit/:filename',function(req,res){
+   res.render('edit',{filename:req.params.filename})
+   
+})
+app.get('/delete/:filename',function(req,res){
+  res.render('delete',{filename:req.params.filename})
+})
+
+app.post('/create',function(req,res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+    res.redirect('/')
+   })
+})
+app.post('/edit',function(req,res){
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+        res.redirect('/')
+        
+    })
     
-</body>
-</html>
+})
+app.post('/delete',function(req,res){
+   fs.unlink(`./files/${req.body.previous}`,function(err){
+    res.redirect('/')
+   })
+    
+})
+
+app.listen(3000,function(){
+    console.log('its running')
+})
 
